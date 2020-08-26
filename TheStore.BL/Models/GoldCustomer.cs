3649d1af21bac18c439ca8358756b15f3001266c
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-08-26 07:23:44 +0200

    Creación de dos clases de clientes y herencia de clases

---
+++ TheStore.BL/Models/GoldCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL.Models
+{
+    public class GoldCustomer: Customer
+    {
+        public float Discount { get; set; }
+        public List<string> DiscountCupons { get; set; }
+    }
+}
---
+++ TheStore.BL/Models/SilverCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheStore.BL.Models
+{
+    public class SilverCustomer: Customer
+    {
+        public int RemainingPurchases { get; set; }
+    }
+}
